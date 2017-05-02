5cdf1af50a9fb841ad9af48fc613fc131d055eb2
Christopher Reimer <mail+gitproject@c-reimer.de>
2017-05-02 22:25:08 +0200

    SplitChars now also crops top and bottom

--- classes/preprocessing.py
+++ classes/preprocessing.py
@@ -19,7 +19,7 @@ class Preprocessing:
         block_size = 11  # decides the size of neighbourhood area
         c = -10  # a constant which is subtracted from the mean or weighted mean
         print("Binarising image")
-        ret, img = cv2.threshold(self.img, 100, 255, cv2.THRESH_BINARY)
+        ret, img = cv2.threshold(self.img, 127, 255, cv2.THRESH_BINARY)
         # img = cv2.adaptiveThreshold(self.img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, block_size, c)
 
         # Check if inversion is necessary
@@ -38,43 +38,56 @@ class Preprocessing:
             self.img = img
 
     # Trennung an Spalten mit 0 Pixeln
-    def splitChars(self, occupancyThres=0):
+    def splitChars(self):
         print("Splitting characters")
+        chars = []
 
-        # Zähle genutze Pixel in allen Spalten
         rowOccupancy = [None] * self.rows
         for row in range(self.rows):
-            singleOcc = 0
             for line in range(self.lines):
                 if self.img[line][row] == 255:
-                    singleOcc += 1
-            rowOccupancy[row] = singleOcc
+                    rowOccupancy[row] += 1
 
-        chars = []
-        cur_row = -1
         start_row = None
         end_row = None
-        while cur_row < self.rows:
-            cur_row += 1
-            if start_row and end_row:
-                chars.append(self.img[0:self.lines, start_row:end_row])
+        for row in range(len(rowOccupancy)):
+            if start_row:
+                if not rowOccupancy[row]:
+                    end_row = row
+            else:
+                if rowOccupancy[row]:
+                    start_row = row
 
+            if start_row and end_row:
+                img = self.splitTopBottom(self.img[0:self.lines, start_row:end_row])
+                chars.append(img)
+
+                # Reset
                 start_row = None
                 end_row = None
 
-                continue
-
-            try:
-                if start_row:
-                    if rowOccupancy[cur_row] <= occupancyThres:
-                        end_row = cur_row
-                else:
-                    if rowOccupancy[cur_row] > occupancyThres:
-                        start_row = cur_row
-            except IndexError:
-                break
-
         return chars
+
+    def splitTopBottom(self, img):
+        (lines, rows) = img.shape
+
+        lineOccupancy = [0] * self.lines
+        for line in range(lines):
+            for row in range(rows):
+                if img[line][row] == 255:
+                    lineOccupancy[line] += 1
+
+        start_line = None
+        end_line = None
+        for line in range(len(lineOccupancy)):
+            if start_line:
+                if not lineOccupancy[line]:
+                    end_line = line
+            else:
+                if lineOccupancy[line]:
+                    start_line = line
+            if start_line and end_line:
+                return img[start_line:end_line, 0:rows]
 
     def skelettizeImg(self):
         skel = numpy.zeros(self.img.shape, numpy.uint8)  # Initialize empty skeleton
