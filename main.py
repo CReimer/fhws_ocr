a2f0d1acd7552004d72cb776ddf5890c0e2a85a5
Florian <florian.gresser@student.fhws.de>
2017-05-02 16:54:39 +0200

    Feature extraction method added for testing

--- main.py
+++ main.py
@@ -3,13 +3,21 @@
 import cv2
 from classes.preprocessing import Preprocessing
 from classes.tools import Tools
+from classes.featureExtraction import FeatureExtraction
 
 img = cv2.imread('1_pontifically_58805.jpg', cv2.IMREAD_GRAYSCALE)
 
 test = Preprocessing(img)
 tools = Tools()
 
+test.stretchContrast()
+#tools.showImage(test.img)
 test.binariseImg()
+
+test_flo = FeatureExtraction(test.img)
+test_flo.getpixelaverage()
+
+
 tools.showImage(test.img)
 chars = test.splitChars()
 
