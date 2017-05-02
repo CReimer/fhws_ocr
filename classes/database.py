ada32240b4a71c563e454068bd88c19e639e4e80
Christopher Reimer <mail+gitproject@c-reimer.de>
2017-05-02 19:58:55 +0200

    Add database class

---
+++ classes/database.py
@@ -0,0 +1,32 @@
+import json
+import string
+
+
+class Database:
+    def __init__(self):
+        self.data = dict()
+
+    def initializeEmpty(self):
+        # Initialize with lower and uppercase chars
+        for char in string.ascii_lowercase + string.ascii_uppercase:
+            self.data[char] = dict()
+
+    def add(self, char, type, data):
+        # Add 'data' of 'type' to some 'char'
+        try:
+            self.data[char][type].append(data)
+        except KeyError:
+            self.data[char][type] = list()
+            self.data[char][type].append(data)
+
+    def read(self, char, type):
+        # Read 'data' of 'type' from some 'char'. Returns list()
+        return self.data[char][type]
+
+    def saveDatabase(self):
+        with open('data.json', 'w') as fp:
+            json.dump(self.data, fp)
+
+    def loadDatabase(self):
+        with open('data.json', 'r') as fp:
+            self.data = json.load(fp)
--- main.py
+++ main.py
@@ -3,10 +3,19 @@
 import cv2
 from classes.preprocessing import Preprocessing
 from classes.tools import Tools
+from classes.database import Database
 
 tools = Tools()
+database = Database()
+database.initializeEmpty()
 
-img = cv2.imread('1_pontifically_58805.jpg', cv2.IMREAD_GRAYSCALE)
+# database.loadDatabase()
+database.add('a', 'histogram' "asdfg")
+database.saveDatabase()
+
+exit()
+
+# img = cv2.imread('1_pontifically_58805.jpg', cv2.IMREAD_GRAYSCALE)
 img = cv2.imread('Unbenannt.png', cv2.IMREAD_GRAYSCALE)
 
 tools.showImage(img)
@@ -16,9 +25,10 @@ preprocess.binariseImg()
 img = preprocess.img
 tools.showImage(img, "Before")
 
-chars = preprocess.splitChars()
+# chars = preprocess.splitChars()
 preprocess.skelettizeImg()
 tools.showImage(preprocess.img)
+tools.writeImage(preprocess.img, "test.png")
 
-for idx in range(len(chars)):
-    tools.writeImage(chars[idx], "single" + str(idx) + '.jpg')
+# for idx in range(len(chars)):
+#     tools.writeImage(chars[idx], "single" + str(idx) + '.jpg')
